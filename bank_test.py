fa97b70a87f913dbeebf6c696f44ae1c17528f27
Michael Ladouceur <michaelladouceur1@gmail.com>
2018-07-08 22:53:06 -0500

    Added csv data collector and insert to database

--- bank_test.py
+++ bank_test.py
@@ -16,17 +16,46 @@ def get_Filepath():
 	return files
 
 class Data():
-	def __init__(self):
-		pass
+	def __init__(self,new_files):
+		self.get_Data(new_files)
+
+	def get_Data(self,new_files):
+		hold_data = []
+		self.data = pd.DataFrame()
+		if len(new_files) > 0:
+			for item in new_files:
+				with open(item, 'r') as file:
+					sheet = pd.read_csv(file,index_col=False)
+					hold_data.append(sheet)
+			self.data = pd.concat(hold_data)
+			return self.clean_Data()
+		else:
+			return None
+
+	def clean_Data(self):
+		self.data = self.data.rename(columns={'Posting Date':'postDate'})
+		self.data['postDate'] = pd.to_datetime(self.data.postDate)
+		self.data = self.data.sort_values(by='postDate')
+		self.data['Index'] = range(self.data.shape[0])
+		self.data = self.data.set_index('Index')
+		self.data.drop(columns=to_drop,axis=1,inplace=True)
+		self.data['Description'] = self.data['Description'].str.upper()
+		self.data['Description'] = self.data['Description'].str.translate(str.maketrans(dict.fromkeys('0123456789/#-')))
+		self.data['Description'] = self.data['Description'].str.split(r'\s{2,}')
+		self.data['Description'] = self.data.apply(lambda x: pd.Series(x['Description'][0]),axis=1)
+		for i,j in word_drop.items():
+			self.data['Description'] = self.data['Description'].str.replace(i,j)
+		return self.data
+
 
 class DataBase():
 	def __init__(self,database):
 		self.concur_Database(database)
 		self.create_Tables()
 		self.validate_Statements()
-		data = self.pull_Data('existStatement')
-		for item in data:
-			print(item)
+		data = Data(self.new_files)
+		#self.pd_Insert_Data(data.data, 'bankStatement')
+		print(self.pull_Data('bankStatement', column='description'))
 
 	#Connect to and create cursor object for database
 	def concur_Database(self,database):
@@ -53,15 +82,17 @@ class DataBase():
 		print('Validating...')
 		statements = self.pull_Data('existStatement')
 		files = get_Filepath()
-		if len(statements) != 0:
-			for file in files:
-				if file in statements:
-					continue
-				else:
-					self.insert_Data('existStatement', 'statement', file)
-		else:
-			self.insert_Data('existStatement', 'statement', files)
-		print(f'{len(files)} bank statements found')
+		self.new_files = []
+		for file in files:
+			if file in statements:
+				continue
+			else:
+				self.insert_Data('existStatement', 'statement', file)
+				self.new_files.append(file)
+		print(f'{len(self.new_files)} new bank statements found:')
+		for item in self.new_files:
+			print(item)
+		print('\n')
 
 	#Insert data into database
 	def insert_Data(self,table,column,data):
@@ -71,6 +102,12 @@ class DataBase():
 		else:
 			self.cur.execute(f'''INSERT INTO {table}({column}) VALUES(?)''',(data,))
 		self.conn.commit()
+
+	#Pandas insert data into database
+	def pd_Insert_Data(self, data, table):
+		data.to_sql(table,con=self.conn, if_exists='append', index=False)
+		self.conn.commit()
+		print('Data inserted into the ' + table + ' table')
 
 	#Pull data from database
 	def pull_Data(self,table,column='*'):
